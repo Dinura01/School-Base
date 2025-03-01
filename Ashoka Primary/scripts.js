e930a870f871d2bbad7d0561a8cd864410d425f5
Dinura01 <jayasinghadinura@gmail.com>
2025-03-02 00:06:28 +0530

    Delete Ashoka Primary directory

--- Ashoka Primary/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('.btn-info');
-    buttons.forEach(button => {
-        button.addEventListener('click', function() {
-            alert('You clicked: ' + this.innerText);
-        });
-    });
-});
